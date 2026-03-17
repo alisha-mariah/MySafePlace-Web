3c341e18f1b0b59d23d4b41c7d93011a55dd319a
alisha-mariah <alisha.mungro@my.lr.edu>
2026-03-16 23:34:46 -0400

    Initial Firebase setup and project structure

---
+++ src/lib/firebaseClient.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3aeoNRNXVuliRBlNHr3uoMPTTGOOccqw",
+  authDomain: "mysafeplace-f01ae.firebaseapp.com",
+  projectId: "mysafeplace-f01ae",
+  storageBucket: "mysafeplace-f01ae.firebasestorage.app",
+  messagingSenderId: "968528233756",
+  appId: "1:968528233756:web:ebef9129e0124202a126d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
